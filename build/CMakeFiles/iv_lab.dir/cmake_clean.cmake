b118d9c3dfa0018bea79dcb0a558cba71872d432
Yueyi Wang <yueyiwang@YueyideMacBook-Air.local>
2025-09-28 20:05:32 +0100

    chore: minimal CMake + hello world main

---
+++ build/CMakeFiles/iv_lab.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iv_lab.dir/src/main.cpp.o"
+  "CMakeFiles/iv_lab.dir/src/main.cpp.o.d"
+  "iv_lab"
+  "iv_lab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iv_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(){
+    std::cout << "Build OK" << std::endl;
+    return 0;
+}
